1e23ca3ae3cdcecd26879c46b30fa56b3b5280c0
neonovyj <vx200014@gmail.com>
2022-02-04 23:25:57 +0300

    js1.0

--- js/generator.js
+++ js/generator.js
@@ -1,11 +1,5 @@
-// document.getElementById('generate').onclick = generatePass;
-// let a = 0;
-// //var a = document.getElementById('passLength').oninput.value;
-// function generatePass(){
-//   a++;
-//   console.log(a);
-//   document.getElementById('generate2').innerHTML = a;
-// }
+//let a = 0;
+//var a = document.getElementById('passLength').oninput.value;
 //
 // generatePass();
 //butt.onclick = function() {
@@ -19,18 +13,61 @@
 // lox();
 
 
-
+var pswrd;
 var length;
+var symbols;
 const ASCII_LETTERS = 'abcdefghijklnopqrstuvwxyz';
 const ASCII_UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const DIGITS = '0123456789';
 const PUNCTUATION = '+-/*!&$#?=@<>';
-length = ASCII_LETTERS + DIGITS;
+//length = ASCII_LETTERS + DIGITS;
 
-var pswrd = "";
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
+document.getElementById('generate').onclick = generate;
 
-while (pswrd.length < 6) {
-  pswrd += alphabet[Math.floor(Math.random() * alphabet.length)];
+function generate(){
+  custom();
+  doPswrd();
+  document.getElementById('generate2').innerHTML = pswrd;
 }
-console.log(pswrd);
+
+
+
+
+
+
+function custom(){
+
+symbols = ASCII_LETTERS;
+var chbox_uppercase;
+chbox_uppercase=document.getElementById('uppercase');
+if (chbox_uppercase.checked) {
+  symbols = symbols + ASCII_UPPERCASE;
+		// console.log(symbols);
+	}
+
+var chbox_digit;
+chbox_digit=document.getElementById('digit');
+if (chbox_digit.checked) {
+    symbols = symbols + DIGITS;
+		// console.log(symbols);
+	}
+
+var chbox_special;
+chbox_special=document.getElementById('special');
+if (chbox_special.checked) {
+  symbols = symbols + PUNCTUATION;
+	// console.log(symbols);
+	}
+
+}
+
+
+function doPswrd(){
+  var length = document.getElementById('passLength').value;
+
+
+  while (pswrd.length < length) {
+    pswrd += symbols[Math.floor(Math.random() * symbols.length)];
+  }
+  console.log(pswrd);
+}
