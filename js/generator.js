766d82be800b909672cbe71f55ba64cf9b2cb2a0
neonovyj <vx200014@gmail.com>
2022-02-07 18:58:37 +0300

    js2.0

--- js/generator.js
+++ js/generator.js
@@ -8,39 +8,29 @@ const PUNCTUATION = '+-/*!&$#?=@<>';
 
 
 document.getElementById('generate').onclick = generate;
-
 function generate(){
   custom();
   doPswrd();
-
 }
 
-
 function custom(){
-symbols = ASCII_LETTERS;
+var symbols = ASCII_LETTERS;
 var chbox_uppercase;
 chbox_uppercase=document.getElementById('uppercase');
 if (chbox_uppercase.checked) {
   symbols = symbols + ASCII_UPPERCASE;
-		// console.log(symbols);
 	}
-
 var chbox_digit;
 chbox_digit=document.getElementById('digit');
 if (chbox_digit.checked) {
     symbols = symbols + DIGITS;
-		// console.log(symbols);
 	}
-
 var chbox_special;
 chbox_special=document.getElementById('special');
 if (chbox_special.checked) {
   symbols = symbols + PUNCTUATION;
-	// console.log(symbols);
 	}
-
 }
-
 
 function doPswrd(){
   var pswrd = '';
@@ -48,10 +38,7 @@ function doPswrd(){
   if (length1 > 25){
     length1 = 24;
   }
-  //console.log(length1);
   while (pswrd.length < length1) {
     pswrd += symbols[Math.floor(Math.random() * symbols.length)];
   }
-  document.getElementById('generate2').innerHTML = pswrd;
-  //console.log(pswrd);
 }
